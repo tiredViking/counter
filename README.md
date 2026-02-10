838317c77278f9efe18fc2879820f8cf4946e717
G V <reecegaines44@gmail.com>
2026-02-11 00:10:29 +0100

    First upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# WEB APP COUNTER PROJECT
+## Start2Impact - Master in Fullstack Development
+
+### Visit the site at [tiredviking.github.io/counter](https://tiredviking.github.io/counter/).
+### Overview
+- This web app was created as a project for Start2Impact Master in Fullstack Development. It is an app that allows the user to increase or decrease a counter, based on a selected step.  
+Here is a screenshot of the UI:  
+![](img/screenshot_20012026_004947.jpg)
+
+- The interface is generated dinamically using JavaScript;
+- The counter can have negative numbers;
+- The step can only be an integer between 1 and 100; floats are not allowed;
+- Input validation for the step is done when clicking on the + or - button; if the input is invalid an alert is displayed on screen;
+- Clicking on the reset button sets the counter to 0, but does not reset the step to 1;
+- Reloading the page resets the counter to 0 and the step to 1.
---
+++ js/script.js
@@ -0,0 +1,115 @@
+// SCRIPT THAT CREATES A WEB APP THAT ALLOWS THE USER
+// TO ADD OR DECREASE A COUNTER, WITH A DEFAULT STEP OF 1,
+// OR RESET THE COUNTER.
+// THE STEP CAN ONLY BE A NUMBER BETWEEN 1 AND 100;
+// THE COUNTER CAN BECOME NEGATIVE;
+// RELOADING THE PAGE RESETS THE COUNTER TO 0 AND STEP TO 1.
+
+document.addEventListener('DOMContentLoaded', () => {
+    // GLOBAL VARIABLES 
+    var counter_value = 0;
+
+    // HTML ELEMENTS 
+    
+    const description_div = document.createElement("div");
+    const description_title = document.createElement("h1");
+    const description_text = document.createElement("p");
+    const app_container = document.createElement("div");
+    const counter_div = document.createElement("div");
+    const operations_div = document.createElement("div");
+    const increment_btn = document.createElement("button");
+    const decrement_btn = document.createElement("button");   
+    const step_div = document.createElement("div");
+    const step_label = document.createElement("label");
+    const step_input = document.createElement("input");
+    const reset_btn = document.createElement("button");
+
+    //ADD CLASS,TEXT, AND APPEND CHILD
+
+    description_div.className = "description_div";
+    description_title.textContent = "Welcome!";
+    description_text.textContent = "This app is a web counter; you can increase or decrease the number below by clicking on + or -, and the count will be modified by the step you select (the default step is one). The step is only valid when it is an integer between 1 and 100. You can also reset the counter to zero by cliking on the the prompt below."
+
+    app_container.className = "app_container";
+
+    counter_div.className = "counter";
+    counter_div.textContent = counter_value;
+
+    operations_div.className = "operations";
+
+    decrement_btn.className = "decrease";
+    decrement_btn.textContent = "-";
+
+    increment_btn.className = "increase";
+    increment_btn.textContent = "+";
+
+    step_div.className = "step";
+    //STEP INPUT FORM
+    step_input.className = "step_input";
+    step_input.id = "step_form"
+    step_input.type = "number";
+    step_input.name = "step_input_form";
+    step_input.min = 1;
+    step_input.max = 100;
+    step_input.value = 1;
+    //STEP_LABEL FOR THE FORM
+    step_label.className = "step_label";
+    step_label.htmlFor = "step_form";
+    step_label.textContent = "Step (1 - 100)";
+
+    reset_btn.className = "reset";
+    reset_btn.textContent = "Reset counter to zero";
+    
+    document.body.appendChild(description_div);
+    description_div.appendChild(description_title);
+    description_div.appendChild(description_text);
+    document.body.appendChild(app_container);
+    app_container.appendChild(counter_div);
+    app_container.appendChild(operations_div);
+    operations_div.appendChild(decrement_btn);
+    operations_div.appendChild(step_div);
+    step_div.appendChild(step_label);
+    step_div.appendChild(step_input);
+    operations_div.appendChild(increment_btn);
+    app_container.appendChild(reset_btn);
+
+    // FUNCTIONS
+
+    function increase(step, counter) {
+        return counter + step;
+    }
+    
+    function decrease(step, counter) {
+        return counter - step;
+    }
+    
+
+    // EVENT LISTENERS
+    // 
+    // IF THE USER CLICKS ON +
+    increment_btn.addEventListener("click", () => { 
+        if (Number(step_input.value) < 1 || Number(step_input.value) > 100 || !Number.isInteger(Number(step_input.value))) {
+            alert("The step can only be an integer between 1 and 100");
+            return;
+        }
+        counter_value = increase(Number(step_input.value), counter_value);
+        counter_div.textContent = counter_value;
+        
+        })
+
+     // IF THE USER CLICKS ON -   
+    decrement_btn.addEventListener("click", () => {
+        if (Number(step_input.value) < 1 || Number(step_input.value) > 100 || Number.isNaN(Number(step_input.value))) {
+            alert("The step can only be an integer between 1 and 100");
+            return;
+        }
+        counter_value = decrease(Number(step_input.value), counter_value)
+        counter_div.textContent = counter_value;
+    })
+
+    // IF THE USER CLICKS THE RESET BUTTON
+     reset_btn.addEventListener("click", () => {
+         counter_value = 0;
+         counter_div.textContent = counter_value;
+     })
+}) // END DOMCONTENTLOADED
